2ec5b28a55fc15194f8c60072924cff84ba73afe
Deividas Gagiskis <deividasdre@gmail.com>
2026-08-20 21:45:44 +0300

    add pagination controls to demo page

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -11,3 +11,13 @@ def test_demo_page_returns_html():
     assert response.status_code == 200
     assert "text/html" in response.headers["content-type"]
     assert "Scrape Job Tracker Demo" in response.text
+
+
+def test_demo_page_has_pagination_controls():
+    response = client.get("/demo")
+
+    assert response.status_code == 200
+    assert 'id="limitSelect"' in response.text
+    assert "Previous" in response.text
+    assert "Next" in response.text
+    assert "Download CSV" in response.text
